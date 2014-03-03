d04c61e66ff2207a245694123a0ad4e42f0f2c83
m0t <m0t@users.noreply.github.com>
2014-03-03 15:07:31 +0000

    pybing

--- pybing.py
+++ pybing.py
@@ -1,17 +1,17 @@
-import urllib
+#import urllib
 import urllib2
 import json
  
  
-def bing_search(query, search_type):
+def bing_search(query, search_type, key):
     #search_type: Web, Image, News, Video
-    key= ''
     query = urllib.quote(query)
     # create credential for authentication
     user_agent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; FDM; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322)'
     credentials = (':%s' % key).encode('base64')[:-1]
     auth = 'Basic %s' % credentials
-    url = 'https://api.datamarket.azure.com/Data.ashx/Bing/Search/'+search_type+'?Query=%27'+query+'%27&$top=5&$format=json'
+    #url = 'https://api.datamarket.azure.com/Data.ashx/Bing/Search/'+search_type+'?Query=%27'+query+'%27&$top=5&$format=json'
+    url = 'https://api.datamarket.azure.com/Bing/Search/v1/Composite?Sources=%27'+search_type+'%27&Query=%27'+query+'%27&$format=json'
     request = urllib2.Request(url)
     request.add_header('Authorization', auth)
     request.add_header('User-Agent', user_agent)
